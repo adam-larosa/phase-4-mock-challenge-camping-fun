64828b30aaa21a88864677a23a37086a44ec0a5f
Adam La Rosa <adam.larosa@flatironschool.com>
2022-05-20 08:42:34 -0700

    Revert "notorious mvp solution"
    This reverts commit 5b7bf09a07fb8eb4ecd872ae1cf08aec8cc8f3e3.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,4 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :not_valid
-
-
-  private
-
-  def not_valid(invalid)
-  
-    render json: { errors: invalid.record.errors.full_messages }, status: 422
-  end
-
-  def not_found(invalid)
-    render json: { error: "#{invalid.model} not found" }, status: 404
-  end
 end
--- app/serializers/camper_show_serializer.rb
+++
@@ -1,4 +0,0 @@
-class CamperShowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age
-  has_many :activities
-end
